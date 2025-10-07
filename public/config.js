c46b65d60fb55982ee98241daa7bb0a4717e8559
Mario Reder <mario.reder@pm.me>
2025-10-07 14:14:50 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -16,7 +16,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_APP_NAME": "Venus Perp DEX",
   "VITE_APP_DESCRIPTION": "Orderly Trading Application",
   "VITE_HAS_PRIMARY_LOGO": "true",
-  "VITE_HAS_SECONDARY_LOGO": "false",
+  "VITE_HAS_SECONDARY_LOGO": "true",
   "VITE_ENABLED_MENUS": "Trading,Markets,Leaderboard,Rewards,Vaults,Portfolio",
   "VITE_CUSTOM_MENUS": "",
   "VITE_ENABLE_CAMPAIGNS": "false",
